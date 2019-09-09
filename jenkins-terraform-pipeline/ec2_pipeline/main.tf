098115b8c4f1cac33971a90671cfb2f2d5e08f5d
prateek-paatni <prateek.paatni@gmail.com>
2019-09-09 11:21:46 +0530

    Dynamic AMI ID

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -2,7 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "server_ami" {
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "example" {
-  ami           = "ami-01ed306a12b7d1c96"
+  ami           = data.aws_ami.server_ami.id
   instance_type = "t2.micro"
 }
